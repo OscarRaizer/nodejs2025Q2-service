85344fd440fe42d9440c838ad88267d551f9a3e6
Oscarraizer <boltenkovs36@gmail.com>
2025-05-29 17:07:44 +0700

    feat: add env with port

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import 'dotenv/config';
 
 async function bootstrap() {
+  const port = process.env.PORT || 3000;
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+  await app.listen(port);
+
+  console.log('Server started at http://localhost:', port);
 }
 bootstrap();
