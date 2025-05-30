b1fd18db8ecf2500471fd1ef623d20122ee57ab5
Oscarraizer <boltenkovs36@gmail.com>
2025-05-31 04:43:26 +0700

    feat: add artist endpoint and GET, PUT working methods

---
+++ src/artist/artist.controller.ts
@@ -0,0 +1,48 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  HttpCode,
+} from '@nestjs/common';
+import { ArtistService } from './artist.service';
+import { CreateArtistDto } from './dto/create-artist.dto';
+import { UpdateArtistInfoDto } from './dto/update-artist.dto';
+
+@Controller('artist')
+export class ArtistController {
+  constructor(private readonly artistService: ArtistService) {}
+
+  @Post()
+  @HttpCode(201)
+  create(@Body() createArtistDto: CreateArtistDto) {
+    return this.artistService.create(createArtistDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.artistService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.artistService.findOne(id);
+  }
+
+  @Patch(':id')
+  update(
+    @Param('id') id: string,
+    @Body() updateArtistDto: UpdateArtistInfoDto,
+  ) {
+    return this.artistService.update(id, updateArtistDto);
+  }
+
+  @Delete(':id')
+  @HttpCode(204)
+  remove(@Param('id') id: string) {
+    return this.artistService.remove(id);
+  }
+}
---
+++ src/artist/artist.service.ts
@@ -0,0 +1,83 @@
+import {
+  Injectable,
+  BadRequestException,
+  NotFoundException,
+} from '@nestjs/common';
+import { CreateArtistDto } from './dto/create-artist.dto';
+import { UpdateArtistInfoDto } from './dto/update-artist.dto';
+import { Artist } from './entities/artist.entity';
+import { v4 as uuidv4, validate as uuidValidate } from 'uuid';
+
+@Injectable()
+export class ArtistService {
+  private artistsDatabase: Artist[] = [];
+
+  create(createartistDto: CreateArtistDto) {
+    if (!createartistDto.name || !createartistDto.grammy) {
+      throw new BadRequestException('Name and Grammy status are required');
+    }
+
+    const newArtist: Artist = {
+      id: uuidv4(),
+      name: createartistDto.name,
+      grammy: createartistDto.grammy,
+    };
+
+    this.artistsDatabase.push(newArtist);
+    return newArtist;
+  }
+
+  findAll() {
+    return this.artistsDatabase.map((artist) => artist);
+  }
+
+  findOne(id: string) {
+    if (!uuidValidate(id)) {
+      throw new BadRequestException('Invalid artist ID format');
+    }
+
+    const artist = this.artistsDatabase.find((artist) => artist.id === id);
+    if (!artist) {
+      throw new NotFoundException('artist not found');
+    }
+
+    return artist;
+  }
+
+  update(id: string, dto: UpdateArtistInfoDto) {
+    if (!uuidValidate(id)) {
+      throw new BadRequestException('Invalid artist ID format');
+    }
+
+    const artistIndex = this.artistsDatabase.findIndex(
+      (artist) => artist.id === id,
+    );
+    if (artistIndex === -1) {
+      throw new NotFoundException('artist not found');
+    }
+
+    const updatedArtist: Artist = {
+      ...this.artistsDatabase[artistIndex],
+      name: dto.name,
+      grammy: dto.grammy,
+    };
+
+    this.artistsDatabase[artistIndex] = updatedArtist;
+    return updatedArtist;
+  }
+
+  remove(id: string) {
+    if (!uuidValidate(id)) {
+      throw new BadRequestException('Invalid artist ID format');
+    }
+
+    const artistIndex = this.artistsDatabase.findIndex(
+      (artist) => artist.id === id,
+    );
+    if (artistIndex === -1) {
+      throw new NotFoundException('Artist not found');
+    }
+
+    this.artistsDatabase.splice(artistIndex, 1);
+  }
+}
